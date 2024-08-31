27b00e7caf28fa08704def8f99cd72644462cc37
juanmapo <juanmaporte@hotmail.com>
2024-08-31 16:32:58 -0300

    Ejercicio 1 completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'labo4';
+  edadUno: number = 0;
+  edadDos: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
+
+  calcular(): void {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  limpiar(): void {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
+
 }
